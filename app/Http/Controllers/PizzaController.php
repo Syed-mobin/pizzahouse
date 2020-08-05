8c43965e663996830a9e77257a9ca6591894419b
unknown <dnicerio@gmail.com>
2020-08-06 01:56:32 +0800

    initial commit

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Pizza;
+
+class PizzaController extends Controller
+{
+  public function index(){
+
+    // $pizzas = Pizza::all();
+    // $pizzas = Pizza::orderBy('name', 'desc')->get();
+    // $pizzas = Pizza::where('type', 'hawaiian')->get();
+    $pizzas = Pizza::latest()->get();
+
+  
+    return view('pizzas.index', [
+      'pizzas' => $pizzas
+    ]);
+  }
+
+  public function create(){
+    return view('pizzas.create');
+  }
+
+  public function show($id){
+    
+    $pizza = Pizza::findOrFail($id);
+
+    return view('pizzas.show', ['pizza' => $pizza]);
+  }
+}
