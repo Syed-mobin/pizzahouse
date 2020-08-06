25861022ea65f5d4411f239cbe7e8dffb59bd801
unknown <dnicerio@gmail.com>
2020-08-07 01:11:09 +0800

    complete pizzahouse app

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -7,13 +7,17 @@ use App\Pizza;
 
 class PizzaController extends Controller
 {
+  // public function __construct(){
+  //   $this->middleware('auth');
+  // }
+
   public function index(){
 
     // $pizzas = Pizza::all();
     // $pizzas = Pizza::orderBy('name', 'desc')->get();
     // $pizzas = Pizza::where('type', 'hawaiian')->get();
     $pizzas = Pizza::latest()->get();
-
+    
   
     return view('pizzas.index', [
       'pizzas' => $pizzas
@@ -22,6 +26,29 @@ class PizzaController extends Controller
 
   public function create(){
     return view('pizzas.create');
+  }
+
+  public function store(){
+
+    $pizza = new Pizza();
+
+    $pizza->name = request('name');
+    $pizza->type = request('type');
+    $pizza->base = request('base');
+    $pizza->toppings = request('toppings');
+    
+    $pizza->save();
+
+    return redirect('/')->with('message', 'Thanks for your order');
+  }
+
+  public function destroy($id){
+    
+    $pizza = Pizza::findOrFail($id);
+
+    $pizza->delete();
+    return redirect('/pizzas');
+
   }
 
   public function show($id){
